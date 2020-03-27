2c0018b1c85f65788daeae424c79ba905c41dcdd
Leandro Martins <lemartins@MacBook-Pro-de-Leandro.local>
2020-03-27 16:52:51 -0300

    Estilização do Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_zoom1.jpg?ts=1584624060&ims=544x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$200,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1000,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
