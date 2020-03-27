7946a8746195d3cf7df0d181b53f04a1e8c46da1
Leandro Martins <lemartins@MacBook-Pro-de-Leandro.local>
2020-03-27 16:05:33 -0300

    Estrutura inicial | Estilos globais | Criando header | Estilização da Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_zoom1.jpg?ts=1584624060&ims=544x"
+          alt="Tenis"
+        />
+        <strong>Tenis legal</strong>
+        <span>R$111,11</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_zoom1.jpg?ts=1584624060&ims=544x"
+          alt="Tenis"
+        />
+        <strong>Tenis legal</strong>
+        <span>R$111,11</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_zoom1.jpg?ts=1584624060&ims=544x"
+          alt="Tenis"
+        />
+        <strong>Tenis legal</strong>
+        <span>R$111,11</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_zoom1.jpg?ts=1584624060&ims=544x"
+          alt="Tenis"
+        />
+        <strong>Tenis legal</strong>
+        <span>R$111,11</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_zoom1.jpg?ts=1584624060&ims=544x"
+          alt="Tenis"
+        />
+        <strong>Tenis legal</strong>
+        <span>R$111,11</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_zoom1.jpg?ts=1584624060&ims=544x"
+          alt="Tenis"
+        />
+        <strong>Tenis legal</strong>
+        <span>R$111,11</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
