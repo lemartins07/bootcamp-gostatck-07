dd8b73a57ad85c48d757328bd8f0dcd5274ab3de
Leandro Martins <leandro.martins.85@gmail.com>
2020-03-27 19:35:13 -0300

    Reactotron + Redux

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,11 @@ import { createStore } from 'redux';
 
 import rootReducer from './modules/rootReducer';
 
+// integrando o redux com o reactotron
+const enhancer =
+  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+
 // cria o store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
